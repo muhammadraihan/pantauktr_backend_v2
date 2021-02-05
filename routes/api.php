314924b8e4e2769afa33dd316597c5b9a68651d2
gh0st <ghostofcordelia@protonmail.com>
2021-02-05 18:55:45 +0000

    chore: Adding pelapor register endpoints

--- routes/api.php
+++ routes/api.php
@@ -14,21 +14,21 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['prefix' => 'v2'], function() {
-    Route::post('login', 'Api\AuthController@login');
     Route::get('login/{provider}', 'Api\AuthController@redirect');
+    Route::post('login', 'Api\AuthController@LoginPelapor');
+    Route::post('register', 'Api\AuthController@RegisterPelapor');
     Route::post('login/{provider}/token','Api\AuthController@getToken');
     Route::post('deploy', 'DeployController@DeployApps');
   });
 
 Route::group(['prefix' => 'v2', 'middleware' => ['jwt']], function() {
-// Route::group(['prefix' => 'v2'], function(){
-    Route::get('checktoken','Api\AuthController@checkToken');
+    Route::get('checktoken','Api\AuthController@CheckToken');
     Route::get('logout', 'Api\AuthController@logout');
     Route::get('pelapor', 'Api\AuthController@pelapor');
     Route::get('jenisLaporan','Api\ReferensiController@getJenisLaporan');
     Route::get('jenisPelanggaran','Api\ReferensiController@getJenisPelanggaran');
     Route::get('jenisApresiasi','Api\ReferensiController@getJenisApresiasi');
     Route::post('lapor','Api\LaporController@lapor');
-    Route::get('laporan','Api\LaporanController@listLaporan');
+    // Route::get('laporan','Api\LaporanController@listLaporan');
 
 });
