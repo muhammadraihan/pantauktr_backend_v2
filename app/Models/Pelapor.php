bd8572cd7d56219972ec19fd67869a623265c086
gh0st <ghostofcordelia@protonmail.com>
2021-02-05 18:53:03 +0000

    fix: Adding log activity and JWT auth support

--- app/Models/Pelapor.php
+++ app/Models/Pelapor.php
@@ -3,15 +3,87 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\CanResetPassword;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
 use App\Traits\Uuid;
 
-class Pelapor extends Model
+class Pelapor extends Authenticatable implements JWTSubject, CanResetPassword
 {
     use HasFactory;
+    use Notifiable;
     use Uuid;
+    use LogsActivity;
     
     protected $fillable = [
         'firstname', 'lastname', 'email', 'password', 'provider', 'avatar', 'reward_point', 'last_login_ip', 'last_login_at'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'id',
+    ];
+
+    /**
+     * The attibutes for logging the event change
+     *
+     * @var array
+     */
+    protected static $logAttributes = ['firstname', 'lastname','name', 'email','password','avatar'];
+
+    /**
+     * Logging name
+     *
+     * @var string
+     */
+    protected static $logName = 'pelapor';
+
+    /**
+     * Logging only the changed attributes
+     *
+     * @var boolean
+     */
+    protected static $logOnlyDirty = true;
+
+    /**
+     * Prevent save logs items that have no changed attribute
+     *
+     * @var boolean
+     */
+    protected static $submitEmptyLogs = false;
+
+    /**
+     * Custom logging description
+     *
+     * @param string $eventName
+     * @return string
+     */
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "Data has been {$eventName}";
+    }
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
 }
