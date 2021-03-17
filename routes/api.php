f0064635fe1657978d3c99a5645c9fa9ea10895a
gh0st <ghostofcordelia@protonmail.com>
2021-03-17 01:18:26 +0000

    refactor: Change some routes name

--- routes/api.php
+++ routes/api.php
@@ -13,26 +13,25 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['prefix' => 'v2'], function() {
+Route::group(['prefix' => 'v2'], function () {
     Route::post('deploy', 'DeployController@DeployApps');
-    Route::post('register','Api\AuthController@RegisterPelapor');
+    Route::post('register', 'Api\AuthController@RegisterPelapor');
     Route::post('login', 'Api\AuthController@LoginPelapor');
-    Route::get('login/{provider}', 'Api\AuthController@redirect');
-    Route::post('login/{provider}/token','Api\AuthController@getToken');
-    Route::get('refresh-token','Api\AuthController@RefreshToken');
-    Route::post('pelapor/forgot-password','Api\AuthController@ResetPasswordOTP');
-    Route::post('pelapor/update-password','Api\AuthController@UpdateForgotPassword');
+    Route::post('refresh-token', 'Api\AuthController@RefreshToken');
+    Route::get('login/{provider}', 'Api\AuthController@RedirectLogin');
+    Route::post('login/{provider}/token', 'Api\AuthController@CreateTokenForSocialLogin');
+    Route::post('pelapor/forgot-password', 'Api\AuthController@ResetPasswordOTP');
+    Route::post('pelapor/update-password', 'Api\AuthController@UpdateForgotPassword');
 });
 
-// Route::group(['prefix' => 'v2', 'middleware' => ['jwt']], function() {
-Route::group(['prefix' => 'v2'], function(){
-    Route::get('checktoken','Api\AuthController@checkToken');
+// Route::group(['prefix' => 'v2', ], function() {
+Route::group(['prefix' => 'v2', 'middleware' => ['jwt']], function () {
+    Route::get('checktoken', 'Api\AuthController@checkToken');
     Route::get('logout', 'Api\AuthController@logout');
     Route::get('/profil/pelapor', 'Api\AuthController@pelapor');
-    Route::get('jenis-laporan','Api\ReferensiController@getJenisLaporan');
-    Route::get('jenis-pelanggaran','Api\ReferensiController@getJenisPelanggaran');
-    Route::get('jenis-apresiasi','Api\ReferensiController@getJenisApresiasi');
-    Route::post('lapor','Api\LaporController@lapor');
-    Route::get('laporan','Api\LaporanController@listLaporan');
-
+    Route::get('jenis-laporan', 'Api\ReferensiController@getJenisLaporan');
+    Route::get('jenis-pelanggaran', 'Api\ReferensiController@getJenisPelanggaran');
+    Route::get('jenis-apresiasi', 'Api\ReferensiController@getJenisApresiasi');
+    Route::post('lapor', 'Api\LaporController@lapor');
+    Route::get('laporan', 'Api\LaporanController@listLaporan');
 });
