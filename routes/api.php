4424458b16f50139b0dab79f5b7e7415a69d7cca
gh0st <ghostofcordelia@protonmail.com>
2021-03-17 20:56:33 +0000

    refactor: Cleaning git conflict

--- routes/api.php
+++ routes/api.php
@@ -14,48 +14,32 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['prefix' => 'v2'], function () {
+    Route::get('logout', 'Api\AuthController@logout');
+    Route::get('login/{provider}', 'Api\AuthController@RedirectLogin');
     Route::post('deploy', 'DeployController@DeployApps');
     Route::post('register', 'Api\AuthController@RegisterPelapor');
     Route::post('login', 'Api\AuthController@LoginPelapor');
     Route::post('refresh-token', 'Api\AuthController@RefreshToken');
-    Route::get('login/{provider}', 'Api\AuthController@RedirectLogin');
     Route::post('login/{provider}/token', 'Api\AuthController@CreateTokenForSocialLogin');
     Route::post('pelapor/forgot-password', 'Api\AuthController@ResetPasswordOTP');
     Route::post('pelapor/update-password', 'Api\AuthController@UpdateForgotPassword');
 });
 
-<<<<<<< HEAD
-Route::group(['prefix' => 'v2', 'middleware' => ['jwt']], function() {
-// Route::group(['prefix' => 'v2'], function() {
-    
-    Route::get('checktoken','Api\AuthController@checkToken');
-    Route::get('logout', 'Api\AuthController@logout');
-    Route::get('/profil/pelapor', 'Api\AuthController@pelapor');
-    
-    Route::get('jenis-laporan','Api\ReferensiController@getJenisLaporan');
-    Route::get('jenis-pelanggaran','Api\ReferensiController@getJenisPelanggaran');
-    Route::get('jenis-apresiasi','Api\ReferensiController@getJenisApresiasi');
-    
-    Route::post('lapor','Api\LaporController@lapor');
-    Route::get('laporan','Api\LaporController@listLaporan');
-    Route::get('laporan/{id}','Api\LaporController@detailLaporan');
-    Route::get('notif-laporan/{id}','Api\LaporController@notifLaporan');
-    
-    Route::get('blog-list','Api\ExternalLinkController@listLink');
-    Route::get('blog-list/{id}','Api\ExternalLinkController@getOneBlog');
-    Route::get('notif','Api\PushNotificationController@notification');
-
-});
-=======
-// Route::group(['prefix' => 'v2', ], function() {
 Route::group(['prefix' => 'v2', 'middleware' => ['jwt']], function () {
+
     Route::get('checktoken', 'Api\AuthController@checkToken');
-    Route::get('logout', 'Api\AuthController@logout');
     Route::get('/profil/pelapor', 'Api\AuthController@pelapor');
+
     Route::get('jenis-laporan', 'Api\ReferensiController@getJenisLaporan');
     Route::get('jenis-pelanggaran', 'Api\ReferensiController@getJenisPelanggaran');
     Route::get('jenis-apresiasi', 'Api\ReferensiController@getJenisApresiasi');
+
     Route::post('lapor', 'Api\LaporController@lapor');
-    Route::get('laporan', 'Api\LaporanController@listLaporan');
+    Route::get('laporan', 'Api\LaporController@listLaporan');
+    Route::get('laporan/{id}', 'Api\LaporController@detailLaporan');
+    Route::get('notif-laporan/{id}', 'Api\LaporController@notifLaporan');
+
+    Route::get('blog-list', 'Api\ExternalLinkController@listLink');
+    Route::get('blog-list/{id}', 'Api\ExternalLinkController@getOneBlog');
+    Route::get('notif', 'Api\PushNotificationController@notification');
 });
->>>>>>> develop
