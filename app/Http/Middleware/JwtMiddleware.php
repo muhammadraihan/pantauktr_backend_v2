6669bfd90221b851409114249813300a1fe7fd98
gh0st <ghostofcordelia@protonmail.com>
2021-03-17 01:17:27 +0000

    fix: Fixing token json response

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -9,26 +9,26 @@ use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
 class JwtMiddleware extends BaseMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-      try {
-        $pelapor = JWTAuth::parseToken()->authenticate();
-      } catch (Exception $e) {
-        if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-          return response()->json(['status' => 'TOKEN_IS_INVALID'],500);
-        } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-          return response()->json(['status' => 'TOKEN_IS_EXPIRED',500]);
-        } else {
-          return response()->json(['status' => 'TOKEN_NOT_FOUND',500]);
-        }
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+    try {
+      $pelapor = JWTAuth::parseToken()->authenticate();
+    } catch (Exception $e) {
+      if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+        return response()->json(['success' => false, 'message' => 'TOKEN_IS_INVALID']);
+      } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+        return response()->json(['success' => false, 'message' => 'TOKEN_IS_EXPIRED']);
+      } else {
+        return response()->json(['success' => false, 'message' => 'TOKEN_NOT_FOUND']);
       }
-      return $next($request);
     }
+    return $next($request);
+  }
 }
