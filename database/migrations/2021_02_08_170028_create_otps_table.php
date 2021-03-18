db952020a8efeddba2741b588efb446871519994
gh0st <ghostofcordelia@protonmail.com>
2021-03-18 23:13:24 +0000

    chore: Adding drop when migrate rollback

--- database/migrations/2021_02_08_170028_create_otps_table.php
+++ database/migrations/2021_02_08_170028_create_otps_table.php
@@ -20,4 +20,13 @@ class CreateOtpsTable extends Migration
             $table->timestamps();
         });
     }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otps');
+    }
 }
