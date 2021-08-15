473478cfb7fefc1b4d7a55ad11e88983015c33ac
vagrant <vagrant@homestead>
2021-08-15 17:34:27 +0000

    add kawasan crud

---
+++ resources/views/kawasan/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.page')
+
+@section('title', 'Kawasan Create')
+
+@section('css')
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-xl-6">
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>Add New <span class="fw-300"><i>Kawasan</i></span></h2>
+                <div class="panel-toolbar">
+                    <a class="nav-link active" href="{{route('kawasan.index')}}"><i class="fal fa-arrow-alt-left">
+                        </i>
+                        <span class="nav-link-text">Back</span>
+                    </a>
+                    <button class="btn btn-panel" data-action="panel-fullscreen" data-toggle="tooltip"
+                        data-offset="0,10" data-original-title="Fullscreen"></button>
+                </div>
+            </div>
+            <div class="panel-container show">
+                <div class="panel-content">
+                    <div class="panel-tag">
+                        Form with <code>*</code> can not be empty.
+                    </div>
+                    {!! Form::open(['route' => 'kawasan.store','method' => 'POST','class' =>
+                    'needs-validation','novalidate']) !!}
+                    <div class="form-group col-md-6 mb-3">
+                        {{ Form::label('name','Kawasan',['class' => 'required form-label'])}}
+                        {{ Form::text('name',null,['placeholder' => 'Kawasan','class' => 'form-control '.($errors->has('name') ? 'is-invalid':''),'required', 'autocomplete' => 'off'])}}
+                        @if ($errors->has('name'))
+                        <div class="invalid-feedback">{{ $errors->first('name') }}</div>
+                        @endif
+                    </div>
+                <div
+                    class="panel-content border-faded border-left-0 border-right-0 border-bottom-0 d-flex flex-row align-items-center">
+                    <button class="btn btn-primary ml-auto" type="submit">Submit</button>
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('js')
+@endsection
