d99fcaed5136b993cd3df14eda0cc88940583dd0
gh0st <ghostofcordelia@protonmail.com>
2021-09-11 08:47:16 +0700

    refactor: Remove JWT helper

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -3,8 +3,6 @@
 namespace App\Helper;
 
 use Carbon\Carbon;
-use Exception;
-use JWTAuth;
 
 class Helper
 {
@@ -23,24 +21,6 @@ class Helper
       return 'Selamat Siang';
     }
     return 'Selamat Malam';
-  }
-
-  public static function pelapor()
-  {
-    try {
-      if (!$pelapor = JWTAuth::parseToken()->authenticate()) {
-        return response()->json(['status' => 'ACCOUNT_NOT_FOUND'], 404);
-      }
-    } catch (Exception $e) {
-      if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
-        return response()->json(['status' => 'TOKEN_IS_INVALID'], 500);
-      } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-        return response()->json(['status' => 'TOKEN_IS_EXPIRED'], 500);
-      } else {
-        return response()->json(['status' => 'TOKEN_NOT_FOUND'], 500);
-      }
-    }
-    return $pelapor;
   }
 
   public static function GenerateReportNumber($length = 20)
