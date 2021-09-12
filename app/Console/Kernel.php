3655820a22a03e9b996ca3ba398e431d1133c20e
gh0st <ghostofcordelia@protonmail.com>
2021-09-13 05:03:27 +0700

    feat: Adding schedule command to prune revoked and expired token

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        // prune expired or revoked token
+        $schedule->command('passport:purge')->daily();
     }
 
     /**
